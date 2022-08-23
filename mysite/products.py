493a06c711d4a8da0e98df2a886b4381ff5471c9
Thomas Wright <twright@hey.com>
2022-08-23 01:04:46 +0000

    Update control flow for missing product inventory

--- mysite/products.py
+++ mysite/products.py
@@ -22,12 +22,12 @@ def products(data_file):
     newprodsheet["N1"] = "SellOnline"
     newprodsheet["O1"] = "Professional"
     newprodsheet["P1"] = "AddOn"
-    
+
     prodbook = load_workbook(data_file)
     prodsheet = prodbook.active
     prodsheet.delete_cols(idx=15, amount=9)
     prodsheet.delete_cols(idx=5, amount=1)
-    
+
     mr = prodsheet.max_row
     mc = prodsheet.max_column
 
@@ -35,37 +35,35 @@ def products(data_file):
         for colz in range(1, mc + 1):
             tocopy = prodsheet.cell(row=rowz + 2, column=colz)
             newprodsheet.cell(row=rowz + 1, column=colz).value = tocopy.value
-    
+
     response = HttpResponse(content=save_virtual_workbook(newprodbook), content_type='application/ms-excel')
     response['Content-Disposition'] = 'attachment; filename=products-output.xlsx'
     return response
 
 
 def inventory(data_file):
-    prodbook = load_workbook(data_file)
-    prodsheet = prodbook.active
+    inventory = load_workbook(data_file)
+    inventory = inventory.active
 
-    invprodbook = Workbook()
-    invprodsheet = invprodbook.active
-    invrows = prodsheet.max_row
+    invoutput = Workbook()
+    invoutsheet = invoutput.active
+    invrows = inventory.max_row
 
     for idx in range(1, invrows + 1):
-        barcode = prodsheet.cell(row=idx + 2, column=9)
-        invprodsheet.cell(row=idx, column=1).value = barcode.value
+        barcode = inventory.cell(row=idx + 2, column=9)
+        invoutsheet.cell(row=idx, column=1).value = barcode.value
 
-        stock = prodsheet.cell(row=idx + 2, column=15).value
+        stock = inventory.cell(row=idx + 2, column=15).value
 
-        if stock == "Unlimited":
-            invprodsheet.cell(row=idx, column=2).value = 999
-        elif stock == None:
-            break
-        else:
-            if int(stock) < 0:
-                invprodsheet.cell(row=idx, column=2).value = 0
-            else:
-                invprodsheet.cell(row=idx, column=2).value = int(stock)
-                
-    response = HttpResponse(content=save_virtual_workbook(invprodbook), content_type='application/ms-excel')
+        if stock == None or int(stock) <= 0:
+            invoutsheet.cell(row=idx, column=2).value = 0
+        elif stock == "Unlimited":
+            invoutsheet.cell(row=idx, column=2).value = 999
+        elif int(stock) > 0:
+            invoutsheet.cell(row=idx, column=2).value = stock
+
+
+    response = HttpResponse(content=save_virtual_workbook(invoutput), content_type='application/ms-excel')
     response['Content-Disposition'] = 'attachment; filename=inventory-output.xlsx'
     return response
-    
+
